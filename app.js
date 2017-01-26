c06d454abd48c184a43522bc0a4dec7ef5c884a4
ppmakeitcount <pramod.potdar1@gmail.com>
2017-01-26 01:48:52 +0000

    logic complete

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ var Resto = require("./restos");
 var passport = require("passport");
 var mongoose = require("mongoose");
 var session = require("express-session");
+var NodeGeocoder = require('node-geocoder');
 
+var options = {
+    provider: 'google',
+
+    // Optional depending on the providers 
+    httpAdapter: 'https', // Default 
+    apiKey: 'AIzaSyDliVHqw1Sief3cEcVmnHtrKdq0MY03p7k', // for Mapquest, OpenCage, Google Premier 
+    formatter: null // 'gpx', 'string', ... 
+};
+var geocoder = NodeGeocoder(options);
 const MongoStore = require('connect-mongo')(session);
 
 
@@ -61,148 +71,158 @@ app.use(function(req, res, next) {
 
 
 
-app.get('/', function(req, res) {
-    console.log("searchterm" + req.session.search)
-        //req.session.destroy();
-    req.session.restos.forEach(function(r) {
-                console.log("r.resto_id"+r.resto_id);
-                Resto.findOne({
-                    'resto_id': r.resto_id
-                    
-                }, function(err, doc) {
-                    if (err) res.send("something went wrong in databas");
-                    if (doc) {
-                        r.going = doc.totalGoing;
-                    }
-                })
-            })
+function updateResto(req, res, next) {
+    if(req.session.restos){
+    for (var i = 0; i < req.session.restos.length; i++) {
+        console.log("i = " + i);
+        Resto.findOne({
+            'resto_id': req.session.restos[i].resto_id
+        }, function(err, doc) {
+            console.log("req.session.restos[i]" + req.session.restos[i])
+            if (err) console.log(err);
+            if (doc) {
+                console.log(doc);
+            }
+        })
+    }
+    return next();
+    }else{
+        return next();
+    }
+}
 
+
+app.get('/', updateResto, function(req, res) {
+    console.log("searchterm" + req.session.restos)
+    console.log("login" + res.locals.login)
+    
+    if(req.session.restos){
+        
+    
     res.render('index', {
         restos: req.session.restos,
         searchterm: req.session.search,
+        user: req.user
     })
+    }else{
+        res.render('index');
+    }
+    
 })
 
-app.get('/show/user', function(req, res) {
-    User.find({}).then(function(doc) {
-        res.send(doc)
-    })
-})
+app.get('/api/restos/:city', function(req, res) {
+    var latitude, longitude;
+    geocoder.geocode(req.params.city, function(err, result) {
+        console.log(result);
+        req.session.search = req.params.city;
+        latitude = result[0].latitude;
+        longitude = result[0].longitude;
 
-
-app.post('/search', function(req, res) {
-    var latitue = req.body.city.lat;
-    var longitute = req.body.city.lng;
-    client.getGeocode({
-        lat: latitue, //latitude 
-        lon: longitute //longitude 
-    }, function(err, result) {
-        if (!err) {
-            var json = JSON.parse(result);
-            var data = json.nearby_restaurants
-            var resto = [];
-            var going = 0;
-            data.forEach(function(r) {
-                going = 0;
-                var temp = {
-                    "resto_id": r.restaurant.id,
-                    "name": r.restaurant.name,
-                    "cuisines": r.restaurant.cuisines,
-                    "logo": r.restaurant.featured_image,
-                    "going": going,
-                }
-                resto.push(temp);
-            })
-            req.session.restos = null;
-            req.session.search = null;
-            req.session.restos = resto;
-            req.session.search = req.body.searchterm;
-            //console.log("searchterm: " + req.session.search
-            res.sendStatus(200);
-        }
-        else {
-            //console.log(err);
-            res.send(err);
-        }
-    });
-
-})
-
-app.post('/add/resto',isLoggedIn, function(req, res) {
-
-    Resto.findOne({
-        'resto_id': req.body.resto_id
-    }, function(err, doc) {
-        if (err) {
-            console.error(err);
-            res.sendStatus('400')
-        }
-
-        else if (doc) {
-            if (doc.customers.indexOf(req.user._id) >= 0) {
-                doc.totalGoing -= 1;
-                doc.customers = doc.customers.filter(function(f) {
-                    f != req.user._id;
-                });
-                doc.save();
-                req.session.restos.forEach(function(r) {
-                    if (r.resto_id == doc.resto_id) {
-                        r.going = doc.totalGoing;
+        client.getGeocode({
+            lat: latitude, //latitude 
+            lon: longitude //longitude 
+        }, function(err, result) {
+            if (!err) {
+                var json = JSON.parse(result);
+                var data = json.nearby_restaurants
+                var resto = [];
+                var going = 0;
+                data.forEach(function(r) {
+                    going = 0;
+                    var temp = {
+                        "resto_id": r.restaurant.id,
+                        "name": r.restaurant.name,
+                        "cuisines": r.restaurant.cuisines,
+                        "logo": r.restaurant.featured_image,
+                        "going": going,
                     }
+                    resto.push(temp);
                 })
+                req.session.restos = null;
+                req.session.search = null;
+                req.session.restos = resto;
+                req.session.search = req.params.city;
                 res.redirect('/');
             }
             else {
-                doc.totalGoing += 1;
-                doc.customers.push(req.user._id);
-                doc.save();
-                req.session.restos.forEach(function(r) {
-                    if (r.resto_id == doc.resto_id) {
-                        r.going = doc.totalGoing;
-                    }
-                })
-                res.redirect('/');
+                //console.log(err);
+                res.send(err);
+            }
+        });
+    });
+
+})
+
+app.post('/add/resto', function(req, res) {
+    if (!res.locals.login) {
+        console.log("checkin auth");
+        res.redirect('/');
+    }
+    else {
+        console.log("body restoid: " + req.body.resto_id);
+        Resto.findOne({
+            'resto_id': req.body.resto_id
+        }, function(err, doc) {
+            if (err) {
+                console.error(err);
+                res.sendStatus('400')
             }
 
-        }
-        else {
-            var newResto = new Resto();
-            newResto.resto_id = req.body.resto_id
-            newResto.customers.push(req.user._id)
-            newResto.totalGoing = 1
+            else if (doc) {
+                if (doc.customers.indexOf(req.user._id) >= 0) {
+                    doc.totalGoing -= 1;
+                    doc.customers = doc.customers.filter(function(f) {
+                        f != req.user._id;
+                    });
+                    doc.save();
+                    req.session.restos.forEach(function(r) {
+                        if (r.resto_id == doc.resto_id) {
+                            r.going = doc.totalGoing;
+                            console.log("ssaved and incremented")
 
-
-            newResto.save(function(err) {
-                if (err) {
-                    throw err;
+                        }
+                    })
+                    res.redirect('/');
                 }
-                req.session.restos.forEach(function(r) {
-                    if (r.resto_id == req.body.resto_id) {
-                        r.going = 1;
+                else {
+                    doc.totalGoing += 1;
+                    doc.customers.push(req.user._id);
+                    doc.save();
+                    req.session.restos.forEach(function(r) {
+                        if (r.resto_id == doc.resto_id) {
+                            r.going = doc.totalGoing;
+                            console.log("ssaved and incremented")
+                        }
+                    })
+
+                    res.redirect('/');
+                }
+
+            }
+            else {
+                var newResto = new Resto();
+                newResto.resto_id = req.body.resto_id
+                newResto.customers.push(req.user._id)
+                newResto.totalGoing = 1
+
+
+                newResto.save(function(err) {
+                    if (err) {
+                        throw err;
                     }
-                })
-                res.redirect('/');
-            });
-        }
-    });
-
+                    req.session.restos.forEach(function(r) {
+                        if (r.resto_id == req.body.resto_id) {
+                            r.going = 1;
+                            console.log("added and incremented")
+                        }
+                    })
+                    res.redirect('/');
+                });
+            }
+        });
+    }
 })
-app.get('/justsearch', function(req, res) {
-    client.getGeocode({
-        lat: "28.613939", //latitude 
-        lon: "77.209021",
-    }, function(err, result) {
-        if (!err) {
-
-            res.send(result);
-        }
-        else {
-            //console.log(err);
-            res.send(err);
-        }
-    });
-
-})
+// authentication
 
 app.get('/login', passport.authenticate('twitter'));
 
@@ -210,11 +230,9 @@ app.route('/auth/twitter/callback')
     .get(passport.authenticate('twitter', {
         successRedirect: '/',
         failureRedirect: '/donowhere'
-}));
-
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated){
-        return next();
-    }
-    res.sendStatus(504);
-}
+    }));
+app.get('/logout', function(req, res) {
+    req.logout();
+    req.session.destroy()
+    res.redirect('/')
+})
--- public/homepage.js
+++ public/homepage.js
@@ -1,49 +1,13 @@
-$(document).ready(function() {
 
-
-  /*$("#restaurants").on("click", ".going", function() {
-
-          $(this).toggleClass('btn-success btn-danger');
-          if ($(this).text() == 'Going') {
-            $(this).text('Not Going');
-          }
-          else {
-            $(this).text('Going')
-          }
-  });*/
-
-  
-  $('#searchBtn').click(function() {
+$('#searchBtn').click(function() {
     var searchTerm = $("#search").val();
-    $('#restaurants').html(' ');
-
+    console.log(searchTerm)
     $.ajax({
-      url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + searchTerm + '&key=AIzaSyDliVHqw1Sief3cEcVmnHtrKdq0MY03p7k',
-      dataType: "json",
-      success: function(data) {
-        var location = data.results[0].geometry.location;
-        var item = {
-          "city": location,
-          "searchterm": searchTerm
-        }
-        $.ajax({
-          type: 'POST',
-          data: JSON.stringify(item),
-          contentType: 'application/json',
-          url: '/search',
-          success: function(data) {
-            console.log('success');
-            //location.reload();
-            window.location = "/"
-            //console.log(data);
-            // data.forEach(function(r) {
-            //   var list = '<form action="/add/resto" method="post"><div class="media col-md-8 col-md-offset-2 sizeing"><div class="media-left"><a href="#"><img class="media-object" src="' + r.logo + '" width="100px" height="100"></a></div><div class="media-body"><h2 class="media-heading">' + r.name + '</h2>' + r.cuisines + '</div><div class="media-right"><input type="text" class="hidden" value="'+r.resto_id+'" name="resto_id"><button type="submit" class="going btn btn-success" style="width:100px; height:100px"><span class="count">0</span> Going</button></div></div>'
-            //   $('#restaurants').append(list);
-            // })
-          }
-        });
-      }
+      type: "GET",
+      url: "/api/restos/" + searchTerm,
+
+    }).done(function(data) {
+      location.reload();
     })
 
   });
-});
